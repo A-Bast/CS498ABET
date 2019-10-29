01e96a482cf6761f002ce0fb2f3569e7c0122d3f
A-Bast <49210515+A-Bast@users.noreply.github.com>
2019-10-28 20:03:44 -0400

    Update course_portfolio.js

--- src/main/lib/course_portfolio.js
+++ src/main/lib/course_portfolio.js
@@ -1,4 +1,8 @@
 const Portfolio = require('../models/CoursePortfolio')
+const Course = require('../models/Course')
+const User = require('../models/User')
+const Term = require('../models/Term')
+const StudentLearningOutcome = require('../models/StudentLearningOutcome')
 
 module.exports.new = async ({
 	department_id,
@@ -11,8 +15,21 @@ module.exports.new = async ({
 	section
 }) => {
 	// TODO
+    console.log(await Course.query().delete().where('number', '=', parseInt(course_number)))
+    console.log(await User.query().delete().where('linkblue_username', '=', 'test'))
+    var course = await Course.query().insert({ department_id: parseInt(department_id), number: parseInt(course_number) });
+    var user = await User.query().insert({ linkblue_username: 'test' })
+    var new_portfolio = Portfolio.query().insert({
+        course_id: course.id,
+        instructor_id: user.id,
+        semester_term_id: 1, //to be changed later
+        num_students: num_students,
+        section: section,
+        year: year
+    })
+    //end addition
 	return {
-		id: 'todo'
+        id: new_portfolio.id
 	};
 }
 
