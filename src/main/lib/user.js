09f57baf91ee6869fd602ea0cb4b6b261591ad95
jlfa228 <56319683+jlfa228@users.noreply.github.com>
2019-11-03 20:50:38 -0500

    Create user.js

---
+++ src/main/lib/user.js
@@ -0,0 +1,24 @@
+const User = require('../models/User')
+const is_whitelisted = async (linkblue_username) => {
+    //REFERENCE: https://uk.instructure.com/courses/1957194/discussion_topics/12306872
+    //This pulls the user from the user table and puts the linkblueusername
+    const user = await User.query()
+	    .findById(linkblue_username)
+    //if the user matches
+    if (user) {
+        //return true
+        return true
+    }
+    //else
+    else {
+        //return false
+        return false
+    }
+}
+
+//This makes the whitelist public
+module.exports.is_whitelisted = is_whitelisted
+
+  //TASK: Check if the username matches the ones in the whitelist
+  //Edited by: Jeremy Farmer
+  //Viewed by: 
