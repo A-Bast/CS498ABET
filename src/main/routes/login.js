2aafc82a0f7d76c0e98fd73eabdb35b0b5a4bc37
jlfa228 <56319683+jlfa228@users.noreply.github.com>
2019-11-03 20:52:31 -0500

    Update login.js

--- src/main/routes/login.js
+++ src/main/routes/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var mustache = require('../common/mustache')
 var router = express.Router();
+//This imports the user library file
+var user_lib = require('../lib/user')
 
 /* GET login page */
 router.get('/', function (req, res, next) {
@@ -12,12 +14,19 @@ router.get('/', function (req, res, next) {
 
 /* POST login page */
 router.post('/', (req, res, next) => {
-	// hard coded username and password
-	if (req.body.username === 'user' && req.body.password === 'password') {
+	// hard coded username and password AND checks if the username is in the user library
+	if (req.body.username === 'user' && req.body.password === 'password' && user_lib.is_whitelisted(req.body.username)) {
 		res.redirect(302, '/course/')
 	} else {
 		res.redirect(302, '/login/')
 	}
 })
 
+/* TEACHER login page */
+
+
 module.exports = router;
+
+  //TASK: Check if the username matches the ones in the whitelist
+  //Edited by: Jeremy Farmer
+  //Viewed by: 
