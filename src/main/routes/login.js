c232d84850dc400dc664c9a24285204ec2da5deb
jlfa228 <56319683+jlfa228@users.noreply.github.com>
2019-12-03 17:33:20 -0500

    Update login.js

--- src/main/routes/login.js
+++ src/main/routes/login.js
@@ -22,12 +22,28 @@ router.post('/', (req, res, next) => {
 	}
 })
 
-/* TEACHER login page */
+/* GET teacher login page */
+router.get('/', function (req, res, next) {
+	res.render('base_template', {
+		title: 'Login',
+		body: mustache.render('login2')
+	})
+})
 
+/* TEACHER login page */
+router.post('/', (req, res, next) => {
+	// hard coded username and password AND checks if the username is in the user library
+	if (req.body.username === 'user' && req.body.password === 'password' && user_lib.is_whitelisted(req.body.username)) {
+		res.redirect(302, '/course/')
+	} else {
+		res.redirect(302, '/login2/')
+	}
+})
 
 module.exports = router;
 
   //TASK: Check if the username matches the ones in the whitelist
+  //TASK2: Create a login for Instructors
   //Edited by: Jeremy Farmer
   //Viewed by: Logan Manns
   //The code looks fine. It appears to check if user and password are in a whitelist which then redirects to the course page.
